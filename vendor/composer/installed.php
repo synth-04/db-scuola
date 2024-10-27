d2ab0505f0e8376f6aab94526334b96f0bc565c8
gabriele <gabriele.dipinto@gmail.com>
2024-10-27 23:29:18 +0100

    removed composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dbscuola/heroku',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '22f38d36841140fe3a1fa4b880330b3311da246a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dbscuola/heroku' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '22f38d36841140fe3a1fa4b880330b3311da246a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
